58306ea97fe65c6ff8e3194c2f8078c4fd2f9e2a
James Briggs <james.briggs@hotmail.com>
2022-01-25 11:00:44 +0700

    updated card to include answer and expand to full context

--- CardComponent/frontend/src/CardComponent.tsx
+++ CardComponent/frontend/src/CardComponent.tsx
@@ -11,6 +11,7 @@ import CardContent from '@mui/material/CardContent';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import { CardActions, Collapse } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import LaunchIcon from '@mui/icons-material/Launch';
 import { Typography } from "@mui/material";
 
 // these handle the expansion animation
@@ -22,19 +23,18 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  margin: 'auto',
+  marginLeft: 'auto',
   transition: theme.transitions.create('transform', {
     duration: theme.transitions.duration.shortest,
   }),
 }));
-
 
 /**
  * This is a React-based component template. The `render()` function is called
  * automatically when your component should be re-rendered.
  */
 class CardComponent extends StreamlitComponentBase<any> {
-  state = { active: true };
+  state = { active: false };
 
   toggleActive = () => {
     console.log(this.state)
@@ -45,42 +45,51 @@ class CardComponent extends StreamlitComponentBase<any> {
 
   public render = (): ReactNode => {
     // pull in context ID, text, and score
-    const cid = this.props.args["cid"]
+    const title = this.props.args["title"]
     const context = this.props.args["context"]
+    const ans_start = this.props.args["ans_start"]
+    const ans_end = this.props.args["ans_end"]
+    const ans_score = this.props.args["ans_score"]
     const score = this.props.args["score"]
+    const url = this.props.args["url"]
     const key = this.props.args["key"]
     const expand = this.state.active
+
+    // extract answer span
+    const pre_answer = context.slice(0, ans_start)
+    const answer = context.slice(ans_start, ans_end)
+    const post_answer = context.slice(ans_end)
 
     // create the card
     return (
       <div style={{ padding: "1rem" }}>
-        
           <Card variant="outlined">
             <CardHeader
-              title={cid}
+              title={title}
               subheader={score}
             />
             <CardContent>
               <Typography variant="body2">
-                {context}
+                {(expand) ? (pre_answer) : ("")}
+                <span
+                  style={{
+                    fontWeight: expand ? "bold" : "normal",
+                    color: expand ? "#f63366" : "#000000"
+                  }}
+                >
+                  {answer}
+                </span>
+                {(expand) ? (post_answer) : ("")}
               </Typography>
             </CardContent>
-            <Collapse 
-              in={expand} 
-              timeout="auto" 
-              unmountOnExit
-              addEndListener={() => Streamlit.setFrameHeight()}
-              onEntered={() => Streamlit.setFrameHeight()}
-              onExited={() => Streamlit.setFrameHeight()}>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                  aside for 10 minutes.
-                </Typography>
-              </CardContent>
-            </Collapse>
             <CardActions disableSpacing>
+              <IconButton
+                aria-label="open source url"
+                href={url}
+                target="_PARENT"
+              >
+                <LaunchIcon />
+              </IconButton>
               <ExpandMore
                 expand={expand}
                 onClick={this.toggleActive}
@@ -90,9 +99,7 @@ class CardComponent extends StreamlitComponentBase<any> {
                 <ExpandMoreIcon />
               </ExpandMore>
             </CardActions>
-            
           </Card>
-        
       </div>
     )
   }
